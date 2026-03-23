707e4f448d304097e3cac0c70a9ec9bb5c800e61
Sowmiya <sowmiya@email.com>
2026-03-23 15:36:54 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res)=>{
- res.send("Hello Render Deployment");
+// Home route
+app.get("/", (req, res) => {
+  res.send("Hello Render Deployment");
 });
 
-app.listen(3000,()=>{
- console.log("Server running");
+// Port configuration
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
